d2225699d8fe8b4e27f55d117af8fbdba84ef19f
Jay <122465785+GoblinJay@users.noreply.github.com>
2025-04-24 22:47:30 -0700

    Yodaa

--- HangMan/obj/Debug/net9.0-windows10.0.19041.0/win-x64/HangMan.AssemblyInfo.cs
+++ HangMan/obj/Debug/net9.0-windows10.0.19041.0/win-x64/HangMan.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HangMan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+be25db575b54c211aac647b41e5e9d6b72224619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5bc69ba120378c59934c28b1450bbe93a10cda11")]
 [assembly: System.Reflection.AssemblyProductAttribute("HangMan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HangMan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
